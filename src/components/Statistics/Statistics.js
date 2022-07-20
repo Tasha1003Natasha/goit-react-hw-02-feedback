7738db8bfa83e19f7455c6d0f46c8ad178035c5a
Tasha1003Natasha <0955712824n@gmail.com>
2022-07-20 22:15:06 +0300

    № 1.2 готово

--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -5,8 +5,6 @@ export class Statistics extends React.Component {
     good: 0,
     neutral: 0,
     bad: 0,
-    total: 0,
-    feedback: 0,
   };
 
   handleClick = event => {
@@ -18,14 +16,23 @@ export class Statistics extends React.Component {
   };
 
   countTotalFeedback = () => {
-    this.setState(prevState => {
-      return { total: prevState.total };
-    });
+    const { good, neutral, bad } = this.state;
+    const sum = good + neutral + bad;
+    return sum;
+  };
+
+  countPositiveFeedbackPercentage = () => {
+    const { good, neutral, bad } = this.state;
+    const sum = good + neutral + bad;
+    const percentage = (good * 100) / sum;
+    return percentage;
   };
 
   render() {
     const { onClick } = this.props;
-    const { good, neutral, bad, total, feedback } = this.state;
+    const { good, neutral, bad } = this.state;
+    const total = this.countTotalFeedback();
+    const feedback = this.countPositiveFeedbackPercentage();
 
     return (
       <div className="statistics">
@@ -66,81 +73,12 @@ export class Statistics extends React.Component {
             <li className="group__item" onClick={this.countTotalFeedback}>
               Total: {total}
             </li>
-            <li className="group__item">Positive feedback: {feedback} %</li>
+            <li className="group__item">
+              Positive feedback: {feedback || 0} %
+            </li>
           </ul>
         </div>
       </div>
     );
   }
 }
-
-// /////////////////1 варіант///////////////////////////
-// export class Statistics extends React.Component {
-//   // staticpropTypes = {
-//   //     ////
-//   // }
-
-//   state = {
-//     good: 0,
-//     neutral: 0,
-//     bad: 0,
-//     // total: 0,
-//     // feedback: 0,
-//   };
-
-//   handleGood = () => {
-//     this.setState(prevState => ({
-//       good: prevState.good + 1,
-//     }));
-//   };
-
-//   handleNeutral = () => {
-//     this.setState(prevState => ({
-//       neutral: prevState.neutral + 1,
-//     }));
-//   };
-
-//   handleBad = () => {
-//     this.setState(prevState => ({
-//       bad: prevState.bad + 1,
-//     }));
-//   };
-
-//   render() {
-//     const { good, neutral, bad } = this.state;
-
-//     return (
-//       <div className="statistics">
-//         <div className="btn">
-//           <h1 className="btn__title">Please leave feedback</h1>
-//           <button type="button" className="btn__good" onClick={this.handleGood}>
-//             Good
-//           </button>
-//           <button
-//             type="button"
-//             className="btn__neutral"
-//             onClick={this.handleNeutral}
-//           >
-//             Neutral
-//           </button>
-//           <button type="button" className="btn__bad" onClick={this.handleBad}>
-//             Bad
-//           </button>
-//         </div>
-
-//         <div className="group">
-//           <h2 className="group__title">Statistics</h2>
-//           <ul className="group__list">
-//             <li className="group__item">Good: {good}</li>
-//             <li className="group__item">Neutral: {neutral}</li>
-//             <li className="group__item">Bad: {bad}</li>
-//             <li className="group__item">Total: {this.state.total} %</li>
-//             <li className="group__item">
-//               Positive feedback: {this.state.feedback} %
-//             </li>
-//           </ul>
-//         </div>
-//       </div>
-//     );
-//   }
-// }
